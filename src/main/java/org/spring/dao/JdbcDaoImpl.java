80bbab827c8290a3eaa9e68e14cf6c1965fcf187
mohit <mohibharti1998@gmail.com>
2022-06-07 22:27:25 +0530

    Update Commit

--- src/main/java/org/spring/dao/JdbcDaoImpl.java
+++ src/main/java/org/spring/dao/JdbcDaoImpl.java
@@ -7,13 +7,15 @@ import java.sql.*;
 public class JdbcDaoImpl {
     public Circle getCircle(int circleId){
 
-
+        Connection connection = null;
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/spring", "root", "Mohit@123");
+
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/spring", "root", "Mohit@123");
 
 
-        PreparedStatement ps = connection.prepareStatement("Select * from circle where id = ?");
+            PreparedStatement ps = connection.prepareStatement("Select * from circle where id = ?");
+
             ps.setInt(1, circleId);
 
             Circle circle = null;
@@ -26,10 +28,17 @@ public class JdbcDaoImpl {
 
             rs.close();
             ps.close();
-            connection.close();
+
             return circle;
         }catch (Exception ex){
             System.out.println(ex);
+        }
+        finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         return null;
